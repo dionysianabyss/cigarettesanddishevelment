33377ce67c47ee70d8d4af6ef2718d4590df028c
Mjollnir Aurelius <mjollniraurelius@gmail.com>
2026-04-15 09:34:25 +0100

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Mjollnir Aurelius",
+    pageTitle: "",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
