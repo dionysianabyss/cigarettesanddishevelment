d35752d9fc6d993dd104206b067b54ad5f9a26cc
Mjollnir Aurelius <mjollniraurelius@gmail.com>
2026-04-15 09:08:29 +0100

    Update footer links to new URLs

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,8 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://theuselessweb.com/",
+      "Discord Community": "https://theuselessweb.com/",
     },
   }),
 }
