8e7ed5f5c768ba310f68b6618a5889e01408c3cf
Mjollnir Aurelius <mjollniraurelius@gmail.com>
2026-04-15 09:25:02 +0100

    Change page title from 'Cigarettes and Dishevelment' to 'Mjollnir Aurelius'

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Cigarettes and Dishevelment",
+    pageTitle: "Mjollnir Aurelius",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
